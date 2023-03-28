9fc9c8153ecacf2f42fdcf7839342cc5965102a4
HinanoAira <aira@hinasense.jp>
2023-03-29 00:16:35 +0900

    refactoring

--- OpenLinkedVariableSpace/OpenLinkedVariableSpace.cs
+++ OpenLinkedVariableSpace/OpenLinkedVariableSpace.cs
@@ -6,42 +6,35 @@ using BaseX;
 
 namespace OpenLinkedVariableSpace
 {
-  public class OpenLinkedVariableSpace : NeosMod
-  {
-    public override string Name => "OpenLinkedVariableSpace";
-    public override string Author => "HinanoAira";
-    public override string Version => "1.0.0";
-    public override string Link => "https://github.com/HinanoAira/OpneLinkedVariableSpace";
-    public override void OnEngineInit()
+    public class OpenLinkedVariableSpace : NeosMod
     {
-      Harmony harmony = new Harmony("jp.hinasense.OpenLinkedVariableSpace");
-      harmony.PatchAll();
-    }
-    [HarmonyPatch(typeof(WorkerInspector))]
-    private static class WorkerInspectorPatches
-    {
-      [HarmonyPostfix]
-      [HarmonyPatch(nameof(WorkerInspector.BuildInspectorUI))]
-      private static void BuildInspectorUIPostfix(Worker worker, UIBuilder ui)
-      {
-        if (!(worker is IDynamicVariable variable))
-          return;
-
-        var namesButton = ui.Button("Open Dynamic Variable Space");
-        namesButton.LocalPressed += (button, data) => OpenSpace(variable, ((Component)worker).Slot);
-        namesButton.RequireLockInToPress.Value = true;
-      }
-      private static void OpenSpace(IDynamicVariable variable, Slot slot)
-      {
-        var spaces = slot.GetComponentsInParents<DynamicVariableSpace>();
-        foreach (DynamicVariableSpace space in spaces)
+        public override string Name => "OpenLinkedVariableSpace";
+        public override string Author => "HinanoAira";
+        public override string Version => "1.0.1";
+        public override string Link => "https://github.com/HinanoAira/OpneLinkedVariableSpace";
+        public override void OnEngineInit()
         {
-          if (Traverse.Create(variable).Field("handler").Field("_currentSpace").GetValue() == space)
-          {
-            InspectorHelper.OpenInspectorForTarget(space, null, true);
-          }
+            Harmony harmony = new Harmony("jp.hinasense.OpenLinkedVariableSpace");
+            harmony.PatchAll();
         }
-      }
+        [HarmonyPatch(typeof(WorkerInspector))]
+        private static class WorkerInspectorPatches
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(nameof(WorkerInspector.BuildInspectorUI))]
+            private static void BuildInspectorUIPostfix(Worker worker, UIBuilder ui)
+            {
+                if (!(worker is IDynamicVariable variable))
+                    return;
+
+                var namesButton = ui.Button("Open Dynamic Variable Space");
+                namesButton.LocalPressed += (button, data) => OpenSpace(variable);
+                namesButton.RequireLockInToPress.Value = true;
+            }
+            private static void OpenSpace(IDynamicVariable variable)
+            {
+                InspectorHelper.OpenInspectorForTarget((IWorldElement)Traverse.Create(variable).Field("handler").Field("_currentSpace").GetValue(), null, true);
+            }
+        }
     }
-  }
 }
